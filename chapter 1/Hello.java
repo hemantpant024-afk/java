a62246a74263c0ec84d57d3baab44e80324bdd65
hemant pant <hemantpant024@email.com>
2026-09-18 13:44:49 +0530

    add this

--- chapter 1/Hello.java
+++ chapter 1/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String a[]) 
+    {
         System.out.println("Hello java!");
     }
 }
+  
---
+++ chapter 1/Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        int x = 5;
+        x = 10; // Reassigning the value of x
+        System.out.println("The value of x is: " + x);
+        System.out.println(x);
+    }
+}
